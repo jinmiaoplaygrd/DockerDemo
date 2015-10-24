d595d24129e9ef5582308f0ec0ff50d345802097
jinmiaoms <jinmiao@hotmail.com>
2015-10-24 10:07:29 +0000

    change testdb to contactdb

--- nodedemo/app.js
+++ nodedemo/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 //var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('testdb:27017/contacts');
+var db = monk('contactdb:27017/contacts');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
